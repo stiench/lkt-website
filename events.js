c6bb234b5f1d3856e84c78e18e42ac662b67b197
sebgerber <sebastien.gerber@quadrum-solutions.ch>
2024-02-07 21:39:55 +0100

    Add events

---
+++ events.js
@@ -0,0 +1,59 @@
+let events = [];
+let members = [];
+
+async function loadEvents() {
+  await fetch('./data/events.json')
+  .then((response) => response.json())
+  .then((json) => events = json);
+}
+async function loadMembers() {
+  await fetch('./data/members.json')
+  .then((response) => response.json())
+  .then((json) => members = json);
+}
+
+function generateEvents() {
+  var tbodyRef = document.getElementById('tableEvents').getElementsByTagName('tbody')[0];
+  var theadRef = document.getElementById('tableEvents').getElementsByTagName('thead')[0].rows[0];
+
+  members = members.filter(member => member.end == "");
+  members.sort(function (first, second) {
+    return second.name - first.name;
+  });
+  members.forEach(function (member) {
+    theadRef.insertCell().outerHTML = "<th width=\"70\">" + member.name + "</th>";
+  });
+
+  events.sort(function (first, second) {
+    let date1 = new Date(first.start.replace(/-/g,'/')).getTime();
+    let date2 = new Date(second.start.replace(/-/g,'/')).getTime();
+    return date2 - date1;
+  });
+
+  events.forEach(function (event) {
+    var newRow = tbodyRef.insertRow();
+
+    newRow.insertCell().outerHTML = "<th>" + event.year + "</th>";
+    newRow.insertCell().outerHTML = "<th>" + event.where + "</th>";
+    newRow.insertCell().outerHTML = "<th>" + event.start + "</th>";
+    newRow.insertCell().outerHTML = "<th>" + event.end + "</th>";
+
+    members.forEach(function (member) {
+      if (event.members.includes(member.name)) {
+        newRow.insertCell().outerHTML = "<th><i class=\"fa fa-check fa-lg\" aria-hidden=\"true\" style=\"color:#008000;\"></i></th>";
+      }else{
+        newRow.insertCell().outerHTML = "<th></th>";
+      }
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  loadEvents()
+  .then(() => loadMembers())
+  .then(() => {
+    generateEvents();
+  })
+
+  console.log("\r\n .----------------.  .----------------.   .----------------.  .----------------.  .----------------. \r\n| .--------------. || .--------------. | | .--------------. || .--------------. || .--------------. |\r\n| |  _________   | || |    ______    | | | | ____    ____ | || |     _____    | || |     ______   | |\r\n| | |  _   _  |  | || |  .\' ___  |   | | | ||_   \\  \/   _|| || |    |_   _|   | || |   .\' ___  |  | |\r\n| | |_\/ | | \\_|  | || | \/ .\'   \\_|   | | | |  |   \\\/   |  | || |      | |     | || |  \/ .\'   \\_|  | |\r\n| |     | |      | || | | |    ____  | | | |  | |\\  \/| |  | || |      | |     | || |  | |         | |\r\n| |    _| |_     | || | \\ `.___]  _| | | | | _| |_\\\/_| |_ | || |     _| |_    | || |  \\ `.___.\'\\  | |\r\n| |   |_____|    | || |  `._____.\'   | | | ||_____||_____|| || |    |_____|   | || |   `._____.\'  | |\r\n| |              | || |              | | | |              | || |              | || |              | |\r\n| \'--------------\' || \'--------------\' | | \'--------------\' || \'--------------\' || \'--------------\' |\r\n \'----------------\'  \'----------------\'   \'----------------\'  \'----------------\'  \'----------------\' ");
+});
